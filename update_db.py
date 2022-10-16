caeada5a0b7854f8b9d639ab2969d129b0645981
RegreTTO <atary_game@mail.ru>
2022-10-16 18:47:47 +0300

    colors updated

--- update_db.py
+++ update_db.py
@@ -4,8 +4,8 @@ from Bd_Scripts import parse_products, parse_recipes
 
 colorama.init()
 
-print(colorama.Fore.BLUE, "PRODUCTS", colorama.Fore.RESET)
+print(colorama.Fore.CYAN, "PRODUCTS", colorama.Fore.RESET)
 parse_products.main()
 
-print(colorama.Fore.BLUE, "RECIPES", colorama.Fore.RESET)
+print(colorama.Fore.CYAN, "RECIPES", colorama.Fore.RESET)
 parse_recipes.main()
